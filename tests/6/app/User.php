fd3b901413b5a1b11be3ec4ed69b48d80d7a5283
Curso Git <marcooperdomo@gmail.com>
2019-12-14 13:58:23 -0600

    Test de setLastName() y getLastName()

--- tests/6/app/User.php
+++ tests/6/app/User.php
@@ -6,15 +6,26 @@ class User
 {
 
   protected $name;
+  protected $lastname;
 
   public function setName( $name )
   {
     $this->name = $name;
   }
 
+  public function setLastName( $lastname )
+  {
+    $this->lastname = $lastname;
+  }
+
   public function getName()
   {
     return $this->name;
+  }
+
+  public function getLastName()
+  {
+    return $this->lastname;
   }
 
 }
--- tests/6/tests/UserTest.php
+++ tests/6/tests/UserTest.php
@@ -6,12 +6,34 @@ use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase 
 {
 
+  /* Refactor */
+    protected $user;
+
+    public function setUp() : void
+    {
+      $this->user = new User;
+    }
+  /*  */
   public function test_i_can_the_name()
   {
-    $user = new User;
-    $user->setName( "Marco" );
+    // $user = new User; /* Se pasa al método setUp */
+    // $user->setName( "Marco" ); /* Refactor */
+    
+    // $this->assertEquals( $user->getName(), "Marco" ); /* Refactor */
 
-    $this->assertEquals( $user->getName(), "Marco" );
+    $this->user->setName( "Marco" );
+    $this->assertEquals( $this->user->getName(), "Marco" );
+  }
+
+  public function test_i_can_the_lastname()
+  {
+    // $user = new User; /* Se pasa al método setUp */
+    // $user->setLastName( "Perdomo" ); /* Refactor */
+    
+    // $this->assertEquals( $user->getLastName(), "Perdomo" ); /* Refactor */
+    
+    $this->user->setLastName( "Perdomo" );
+    $this->assertEquals( $this->user->getLastName(), "Perdomo" );
   }
   
 }
