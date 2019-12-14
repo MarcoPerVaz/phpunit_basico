f2dfeb93b48db8265155f72d8d9ca1bcb7458722
Curso Git <marcooperdomo@gmail.com>
2019-12-14 13:45:54 -0600

    Test de setName() y getName()

---
+++ tests/6/app/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class User
+{
+
+  protected $name;
+
+  public function setName( $name )
+  {
+    $this->name = $name;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+}
--- tests/6/tests/UserTest.php
+++ tests/6/tests/UserTest.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase 
 {
 
+  public function test_i_can_the_name()
+  {
+    $user = new User;
+    $user->setName( "Marco" );
+
+    $this->assertEquals( $user->getName(), "Marco" );
+  }
+  
 }
